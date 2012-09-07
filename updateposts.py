cb743f487e6a0b4211643e47268e2eb9fa1f34f1
jtcramer <jason.t.cramer@gmail.com>
2012-09-07 03:24:51 -0700

    can successfully pull post data from tumblr blog and store in custom xml structure

--- updateposts.py
+++ updateposts.py
@@ -15,18 +15,21 @@ from json import JSONDecoder
 
 # CONSTANTS
 
-SITEUPDATES = '/home/admin/public_html/blog/posts/siteupdates.xml'
-DEVUPDATES = '/home/admin/public_html/blog/posts/devupdates.xml'
+#SITEUPDATES = '/home/admin/public_html/blog/posts/siteupdates.xml'
+#DEVUPDATES = '/home/admin/public_html/blog/posts/devupdates.xml'
 
-BASEHOSTNAME = ''
-APIKEY =''
+SITEUPDATES = 'siteupdates.xml'
+DEVUPDATES = 'devupdates.xml'
+
+BASEHOSTNAME = 'jtcramer.tumblr.com'
+APIKEY ='2uJvbbuYYzv0vnkUoqHKMdWzXukjkaNfF1zPEemVwAFtLrzmn0'
 
 postsets = [SITEUPDATES, DEVUPDATES]
 tags_dict = {'siteupdates':'siteupdates',
  'devupdates':'devupdates'}
 
 def get_filename(post_file):
-    return post_file.name.split(r'/')[-1][-4]
+    return post_file.name.split(r'/')[-1][:-4]
 
 def get_etree(post_file):
     return ET.ElementTree(ET.fromstring(post_file.read()))
@@ -35,33 +38,33 @@ def save_etree(etree, xmlfile):
     xmlfile.seek(0)
     etree.write(xmlfile,"utf-8", True)
 
-def get_posts_uri(tag, limit=20, type = ''):
+def get_posts_uri(tag):
     """ Returns the URI that returns JSON file with posts of a certain tag"""
-    if type:
-        type = r'/' + type
-    limit = str(limit)
+    #if type:
+    #    type = r'/' + type
+    #limit = str(limit)
     tag = tag.replace(' ', '+')
-    return 'http://api.tumblr.com/v2/blog/' + BASEHOSTNAME + '/posts' + type + '?api_key=' + APIKEY + '&limit=' + limit
-
+    return 'http://api.tumblr.com/v2/blog/' + BASEHOSTNAME + '/posts' + '?api_key=' + APIKEY + '&tag=' + tag
 def init_posts(post_filenames):
     """ Initializes XML post files if they do not exist already."""
     result = []
     for f in post_filenames:
         try:
-            with open(f): pass 
-        except:
+            open(f, 'r+b')
+        except IOError:
             init_post(f)
         finally:
-            result.append(init_post(open(f, 'r+b')))
+            result.append(open(f, 'r+b'))
     return result
 
 def init_post(post_filename):
     """ Initializes a single XML post file. """
-    with open(post_filename, 'r+') as post_file:
+    with open(post_filename, 'w+b') as post_file:
         root = ET.Element('postset')
+        etree = ET.ElementTree(root)
         root.attrib['blogname'] = get_filename(post_file)
         root.attrib['tags'] = tags_dict[root.attrib['blogname']]
-        ET.write(post_file, 'UTF-8', True)
+        etree.write(post_file, 'UTF-8', True)
 
 def generate_post(etree):
     """ Adds a post child to the root level node and returns it. """
@@ -81,62 +84,60 @@ def open_post_files():
         initializes them before returning them. """
     return init_posts(postsets)    
 
-def get_text_post(post, post_element):
+def add_text_post(post, post_element):
     add_full_child(post_element, 'title', post['title'])
     add_full_child(post_element, 'body', post['body'])
-
-def get_photo_post(post, post_element):
+def add_photo_post(post, post_element):
     for photo in post["photos"]:
         caption = photo["caption"]
         for size in photo["alt_sizes"]:
             width = size["width"]
             height = size["height"]
             url = size["url"]
-    
 
-
-def get_video_post(post, post_element):
+def add_video_post(post, post_element):
     pass
 
-def get_quote_post(post, post_element):
+def add_quote_post(post, post_element):
     text = post["text"]
     source = post["source"]
 
-def get_link_post(post, post_element):
+def add_link_post(post, post_element):
     title = post["title"]
     url = post[""]
 
-def get_chat_post(post, post_element):
+def add_chat_post(post, post_element):
     pass
 
-def get_audio_post(post, post_element):
+def add_audio_post(post, post_element):
     pass
 
 
 
 def add_post_element(post, etree):
     """ Returns the ElementTree element representing a post."""
-    attrs = {"href" : post["post_url"], "type" : post["type"], "date" : post["date"]}
-    post_element = generate_post(etree)
-    for attr in attrs.items():
-        add_full_child(post_element, attr[0], attr[1])
-
+    attrs = {"href" : post["post_url"], "type" : post["type"], "date" : post["date"]}    
     post_type = attrs["type"]
+    if post_type == 'text':
+        post_element = generate_post(etree)
+        for attr in attrs.items():
+            add_full_child(post_element, attr[0], attr[1])
 
     if post_type == "text":
-        get_text_post(post, post_element)
+        add_text_post(post, post_element)
+
     elif post_type == "photo":
-        get_photo_post(post, post_element)
+        add_photo_post(post, post_element)
     elif post_type == "video":
-        get_video_post(post, post_element)
+        add_video_post(post, post_element)
     elif post_type == "quote":
-        get_quote_post(post, post_element)
+        add_quote_post(post, post_element)
     elif post_type == "link":
-        get_link_post(post, post_element)
+        add_link_post(post, post_element)
     elif post_type == "chat":
-        get_chat_post(post, post_element)
+        add_chat_post(post, post_element)
     elif post_type == "audio":
-        get_audio_post(post_element)
+        add_audio_post(post_element)
     
 def update_posts(post_file):
     """ Updates the post set. """
@@ -151,10 +152,11 @@ def update_posts(post_file):
             new_posts = JSONDecoder().decode(rawjson)["response"]["posts"]
 
             for post in new_posts:
-                get_post_element(post, etree)
+                add_post_element(post, post_tree)
+
             save_etree(post_tree, post_file)
-    except:
-        pass
+    except Exception as e:
+        print(e)
     finally:
         post_file.close()
 
@@ -165,4 +167,4 @@ def main():
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    exit(main())
